3509517c760c78f63459aaccfc6dffa5cd79447a
Victoria Zhdanova <zh.vika2002@mail.ru>
2022-12-06 18:59:03 +0700

    Снежинки

--- lab7.js
+++ lab7.js
@@ -8,10 +8,10 @@ function showAlert () {
     if (userAge <= 17) {
         greeting='Здравствуй, ';
         if (userSex == 'female') {
-            greeting += 'девочка';
+            greeting += 'девочка ';
         }
         else {
-            greeting += 'мальчик';
+            greeting += 'мальчик ';
         }
     }
     else {
@@ -38,9 +38,26 @@ function makeStairs () {
         stairs.innerHTML += '<div class="step"></div>';
     }
     let steps = document.querySelectorAll ('.step');
-    for (let i = 0; i<steps.length; i++){
+    for (let i = 0; i<steps.length; i++) {
         steps [i].style.left = (30 + 10*i) + 'px';
         steps [i].style.top = (300 + 80*i) + 'px';
         steps [i].innerHTML = 'Блок номер ' + Number(i+1);
     }
+}
+function makeSnowfall () {
+    let Snowfall = document.querySelector ('#Snowfall');
+    let stepCount = 30;
+    for (let i = 0; i<stepCount; i++) {
+        Snowfall.innerHTML += '<img class= "Snowfall" src="1pic.png">';
+    }
+    let PictureSnow= document.images;
+    for (let i=0; i<Snowfall.length;i++){
+        PictureSnow [i]. style.left = Math.random()*window.innerWidth + 'px';
+        PictureSnow [i]. style.top = Math.random()*window.innerHeight + 'px';
+        PictureSnow [i]. style.width = Math.random()*100 + 'px';
+    }
+}
+function ALLFunction() {
+makeStairs();
+makeSnowfall();
 }
