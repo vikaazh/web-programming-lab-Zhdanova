a2fecf22c2127bcc181074370f2e4ea33ffdf9ba
Victoria Zhdanova <zh.vika2002@mail.ru>
2022-12-06 17:00:22 +0700

    Лесенка

--- lab7.js
+++ lab7.js
@@ -17,11 +17,12 @@ function showAlert () {
     else {
         greeting='Здравствуйте, ';
         if(userSex == 'female') {
-            greeting += 'госпожа';
+            greeting += 'госпожа ';
         }
         else {
-            greeting += 'господин';
+            greeting += 'господин ';
         }
+    }
     alert(greeting + userName + ' ' + userSurname + '\n'
     + 'Ваш возраст: '+ userAge);
     let r = Math.round(Math.random()*255);
@@ -29,4 +30,17 @@ function showAlert () {
     let b = Math.round(Math.random()*255);
     let color = 'rgb(' + r + ',' + g + ',' + b + ')';
     document.getElementById ('user-data').style.backgroundColor = color;
+}
+function makeStairs () {
+    let stairs = document.querySelector ('#stairs');
+    let stepCount = 20;
+    for (let i = 0; i<stepCount; i++) {
+        stairs.innerHTML += '<div class="step"></div>';
+    }
+    let steps = document.querySelectorAll ('.step');
+    for (let i = 0; i<steps.length; i++){
+        steps [i].style.left = (30 + 10*i) + 'px';
+        steps [i].style.top = (300 + 80*i) + 'px';
+        steps [i].innerHTML = 'Блок номер ' + Number(i+1);
+    }
 }
