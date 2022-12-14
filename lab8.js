9a88a95e06423d9cd6059782648be3b2aa6e14ff
Victoria Zhdanova <zh.vika2002@mail.ru>
2022-12-14 10:58:22 +0700

    Количество дней с даты рождения

--- lab8.js
+++ lab8.js
@@ -1,9 +1,17 @@
 function showDate(params) {
     let out = document.getElementById('today');
     let today = new Date ();
-    out.innerHTML = today.toLocaleString('ru-RU');
+    out.innerHTML = today.toLocaleDateString('ru-RU');
 }
 
-
+function showDaysCount(params) {
+    let today = new Date();
+    let inputDate = document.querySelector('input[type=date]');
+    let birthday = new Date(inputDate.value);
+    let daysCount = (today - birthday)/1000/60/60/24;
+    let result = document.getElementById('result');
+    daysCount = Math.ceil(daysCount); // округляем в меньшую сторону
+    result.innerHTML = ('С даты рождения прошло дней: ' + daysCount);
+}
 
 window.addEventListener('load', showDate);
