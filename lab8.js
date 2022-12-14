053415335c2d93cc065ed0600eb65dcc39183a01
Victoria Zhdanova <zh.vika2002@mail.ru>
2022-12-14 10:10:51 +0700

    Вывод даты

---
+++ lab8.js
@@ -0,0 +1,9 @@
+function showDate(params) {
+    let out = document.getElementById('today');
+    let today = new Date ();
+    out.innerHTML = today.toLocaleString('ru-RU');
+}
+
+
+
+window.addEventListener('load', showDate);
