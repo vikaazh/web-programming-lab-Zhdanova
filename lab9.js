6fc76f80289415683406966fdd66cbcda71eb326
Victoria Zhdanova <zh.vika2002@mail.ru>
2022-12-23 11:41:23 +0700

    Фокус

--- lab9.js
+++ lab9.js
@@ -36,3 +36,20 @@ window.addEventListener('DOMContentLoaded', function () {
     let coords = document.querySelector ('#coords');
     coords.addEventListener('mousemove', showCoords);
 });
+
+function showFocus(event) {
+    event.target.style.color = 'yellow'
+}
+function showBlur(event) {
+    event.target.style.color = 'black'
+}
+
+window.addEventListener('DOMContentLoaded', function () {
+    let texts = document.querySelectorAll ('input[type = "text"]');
+    texts [0]. addEventListener('focus', showFocus);
+    texts [1]. addEventListener('focus', showFocus);
+    texts [2]. addEventListener('focus', showFocus);
+    texts [0]. addEventListener('blur', showBlur);
+    texts [1]. addEventListener('blur', showBlur);
+    texts [2]. addEventListener('blur', showBlur);
+});
