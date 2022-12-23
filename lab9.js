f07483d1ccb3ed77b132296fe82a57df0de03269
Victoria Zhdanova <zh.vika2002@mail.ru>
2022-12-23 10:50:11 +0700

    Вывод координат мыши

--- lab9.js
+++ lab9.js
@@ -28,3 +28,11 @@ function condition(button) {
         button.style.color = 'pink'
     }
 }
+function showCoords(event) {
+    event.target.innerHTML = 'x = ' + event.offsetX + ', y = ' + event.offsetY + ' ' + 
+    (event.ctrlKey? '<b>Ctrl</b>': '<s>Ctrl</s>') + ' ' + (event.shiftKey? '<b>Shift</b>': '<s>Shift</s>')+ ' ' + (event.altKey? '<b>Alt</b>': '<s>Alt</s>');
+}
+window.addEventListener('DOMContentLoaded', function () {
+    let coords = document.querySelector ('#coords');
+    coords.addEventListener('mousemove', showCoords);
+});
